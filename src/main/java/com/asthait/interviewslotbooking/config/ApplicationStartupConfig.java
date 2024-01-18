4234379f547bbecafd071aae809649f8e73f7c86
Mak <dip@tigerit.com>
2024-01-19 04:47:52 +0600

    Added actuator for health metrics check.

--- src/main/java/com/asthait/interviewslotbooking/config/ApplicationStartupConfig.java
+++ src/main/java/com/asthait/interviewslotbooking/config/ApplicationStartupConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+
 @Configuration
 public class ApplicationStartupConfig {
 
--- src/main/java/com/asthait/interviewslotbooking/model/Interviewer.java
+++ src/main/java/com/asthait/interviewslotbooking/model/Interviewer.java
@@ -1,9 +1,6 @@
 package com.asthait.interviewslotbooking.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
