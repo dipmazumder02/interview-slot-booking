ec3a03db68bcf589447be4af31c5a869d92b6773
dip <mailto.dipmazumder@gmail.com>
2024-01-18 16:02:12 +0600

    Initial Commit

---
+++ src/test/java/com/asthait/interviewslotbooking/InterviewSlotBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asthait.interviewslotbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSlotBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
